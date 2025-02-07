4d9ec373b8b072855146de0b3352ecc4fd509cf9
Aeryle <contact.aeryle@pm.me>
2025-02-07 02:22:45 +0100

    :construction: add temporary construction main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,5 @@
-<p>Website in construction...</p>
+<main
+  class="h-screen w-screen flex flex-col justify-center items-center bg-white dark:bg-black text-black dark:text-white"
+>
+  <h1 class="text-3xl text-center">Website in construction...</h1>
+</main>
