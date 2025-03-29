68074a520f71d4a5c7f007049f73c7f5aa9a76e7
Aeryle <contact.aeryle@pm.me>
2025-03-29 01:08:38 +0100

    :truck: duplicate prefixes to players for updated mod

---
+++ src/routes/api/mods/com.aeryle.playerlist/players/+server.ts
@@ -0,0 +1,35 @@
+import { json, type RequestHandler } from '@sveltejs/kit'
+
+interface Player {
+  UUID: string
+  prefixes?: string[]
+  username?: string
+  postfixes?: string[]
+}
+
+const wrapWithColor = (color: string, content: string) => `<color=${color}>${content}</color>`
+
+export const GET: RequestHandler = () => {
+  const players: Player[] = [
+    {
+      UUID: 'aeryle',
+      prefixes: ['👑'],
+      username: wrapWithColor('#ffd700', '{nickname}'),
+    },
+    {
+      UUID: 't1nquen#goat',
+      prefixes: ['🤡'],
+    },
+    {
+      UUID: 'andrew',
+      prefixes: ['👍'],
+      username: wrapWithColor('red', '{nickname}'),
+    },
+    {
+      UUID: 'adam',
+      prefixes: ['🔥'],
+    },
+  ]
+
+  return json(players)
+}
