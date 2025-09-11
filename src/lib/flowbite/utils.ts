2f2a60d72642664ae19bd488d37c448568014110
Aeryle <contact.aeryle@pm.me>
2025-09-11 17:28:17 +0200

    :bug: convert rgb to hex

--- src/lib/flowbite/utils.ts
+++ src/lib/flowbite/utils.ts
@@ -2,18 +2,16 @@ export const rgbToHex = (rgb: string) => {
   return rgb
     .replace('rgb(', '')
     .replace(')', '')
-    .split(',')
+    .split(', ')
     .map(color => parseInt(color))
     .map(color => color.toString(16))
 }
-
-export const wrapColor = (content: string, color: string) => `<color=#${color.replace('#', '')}ff>${content}</color>`
 
 const convertedFontSizes = {
   '1rem': '-1',
   '2rem': '100%',
   '3rem': '+1',
-}
+} as const
 export const validEmojis = [
   '😊',
   '😋',
@@ -34,8 +32,15 @@ export const validEmojis = [
   '☹️',
   '☹️',
 ] as const
+
+export const wrapColor = (content: string, color: string) => {
+  const [r, g, b, a = 1] = rgbToHex(color)
+
+  return `<color=#${r}${g}${b}${a}>${content}</color>`
+}
 export const wrapFontSize = (content: string, size: keyof typeof convertedFontSizes) =>
   `<size=${convertedFontSizes[size]}>${content}</size>`
+
 export const wrapBold = (content: string) => `<b>${content}</b>`
 export const wrapItalic = (content: string) => `<i>${content}</i>`
 export const wrapStrike = (content: string) => `<s>${content}</s>`
@@ -45,5 +50,8 @@ export const wrapSubscript = (content: string) => `<sub>${content}</sub>`
 export const wrapSuperscript = (content: string) => `<sup>${content}</sup>`
 export const wrapEmoji = (content: (typeof validEmojis)[number]) =>
   `<sprite=${validEmojis.findIndex(emoji => emoji === content)}>`
-
-export const stripNewLines = (content: string) => content.replaceAll('<p></p>', '').replaceAll('<br>', '')
+export const stripNewLines = (content: string) =>
+  content //
+    .replaceAll('<p></p>', '')
+    .replaceAll('<br>', '')
+    .replaceAll('<br/>', '')
