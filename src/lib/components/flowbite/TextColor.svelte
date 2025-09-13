809f2b47aa9cc4ae6aec5caeda1a5256fb5d1faa
Aeryle <contact.aeryle@pm.me>
2025-09-13 16:38:39 +0200

    :rotating_light: add ts-ignore to "complex" type

--- src/lib/components/flowbite/TextColor.svelte
+++ src/lib/components/flowbite/TextColor.svelte
@@ -18,6 +18,7 @@
   type Hex = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'a' | 'b' | 'c' | 'd' | 'e' | 'f'
   interface Color {
     name: string
+    // @ts-ignore
     value: `#${Hex}${Hex}${Hex}${Hex}${Hex}${Hex}`
   }
 
