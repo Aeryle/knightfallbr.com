225c79fc93325ea90f9bf761f147733c3064b496
Aeryle <contact.aeryle@pm.me>
2025-09-13 16:40:01 +0200

    :bug: move decodeURIComponent inside try

--- src/routes/toolbox/+page.server.ts
+++ src/routes/toolbox/+page.server.ts
@@ -1,9 +1,9 @@
 import { stripNewLines } from '$lib/flowbite/utils.js'
 
 export const load = ({ url }) => {
-  const content = decodeURIComponent(atob(url.searchParams.get('nickname') ?? ''))
-
   try {
+    const content = decodeURIComponent(atob(url.searchParams.get('nickname') ?? ''))
+
     return { content: stripNewLines(content.trim()) }
   } catch {
     return { content: '' }
